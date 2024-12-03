ba86753bfb2a44b9982596b7c9e761c76087ff01
cosyreii <reishi2023@gmail.com>
2024-12-04 05:06:42 +0800

    Save and Reset Cookies

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
-let header = document.getElementById("one");
-let button = document.getElementById("two");
+let header = document.getElementById("NomText");
+let cookiButton = document.getElementById("CookiButton");
+let resetButton = document.getElementById("resetButton");
 
 let count = 0;
-function clickHandler() {
-    count++;
 
-    //Challenge!: Change text for multiple of 3 "nam", multiple of 5 change it to "num", and if it is a multiple of both 3 and 5 change it to "nem"
-
-    // % means Modulo, it does magic stuff (remainder of a division) - in this case count % 10 = every multiple of 10 returns you back to 0.
-    if (count % 100 == 0) {
-        header.innerText = "yippee " + count.toString() + " cookies!";
-    } else {
-        header.innerText = "nom " + count.toString() + " cookies";
-    }
-
-    //Challenge code:
-    //   if (count % 3 == 0 && count % 5 == 0) {
-    //     header.innerText = "nem " + count.toString();
-    //   } else if (count % 3 == 0) {
-    //     header.innerText = "nam " + count.toString();
-    //   } else if (count % 5 == 0) {
-    //     header.innerText = "num " + count.toString();
-    //   } else {
-    //     header.innerText = "nom " + count.toString();
-    //   }
+function DisplayCount() {
+  // % means Modulo, it does magic stuff (remainder of a division) - in this case count % 10 = every multiple of 10 returns you back to 0.
+  if (count == 0) {
+    header.innerText = "start clicking bitch -Josh";
+  } else if (count % 100 == 0) {
+    header.innerText = "yippee " + count.toString() + " cookies!";
+  } else {
+    header.innerText = "nom " + count.toString() + " cookies";
+  }
 }
 
-button.addEventListener("click", clickHandler);
+try {
+  let saveCount = parseInt(localStorage.getItem("CookieCount"));
+  if (!Number.isNaN(saveCount)) {
+    count = saveCount;
+  }
+  DisplayCount();
+} catch (error) {
+  console.error("failed to load saved cooki count", error);
+}
+
+cookiButton.addEventListener("click", function () {
+  count++;
+  localStorage.setItem("CookieCount", count.toString());
+
+  //Challenge!: Change text for multiple of 3 "nam", multiple of 5 change it to "num", and if it is a multiple of both 3 and 5 change it to "nem"
+
+  //   if (count % 3 == 0 && count % 5 == 0) {
+  //     header.innerText = "nem " + count.toString();
+  //   } else if (count % 3 == 0) {
+  //     header.innerText = "nam " + count.toString();
+  //   } else if (count % 5 == 0) {
+  //     header.innerText = "num " + count.toString();
+  //   } else {
+  //     header.innerText = "nom " + count.toString();
+  //   }
+
+  DisplayCount();
+});
+
+resetButton.addEventListener("click", function () {
+  count = 0;
+  localStorage.removeItem("CookieCount");
+  DisplayCount();
+});
